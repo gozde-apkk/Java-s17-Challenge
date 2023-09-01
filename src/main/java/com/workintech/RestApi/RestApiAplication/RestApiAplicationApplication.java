149c9d2909b4b7ea04e143d94f4c323a100cbb43
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-09-01 16:42:15 +0300

    Spring Boot added

---
+++ src/main/java/com/workintech/RestApi/RestApiAplication/RestApiAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.workintech.RestApi.RestApiAplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiAplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workintech/RestApi/RestApiAplication/RestApiAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.RestApi.RestApiAplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
